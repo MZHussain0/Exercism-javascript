5f794dd2c4dd877fef8f81c1b0509ea45d0abc1d
mzhussain0 <mzhussain092@gmail.com>
2023-10-26 23:09:42 +0530

    few more problems added

---
+++ matching-brackets/matchingBrackets.js
@@ -0,0 +1,23 @@
+﻿/**
+ * Given a string containing brackets [], braces {}, parentheses (), or any combination thereof, verify that any and all pairs are matched and nested correctly.
+ */
+
+const isPaired = (input) => {
+  const stack = [];
+  const pairs = {
+    "]": "[",
+    "}": "{",
+    ")": "(",
+  };
+  for (let char of input) {
+    if (["[", "{", "("].includes(char)) {
+      stack.push(char);
+    } else if (["]", "}", ")"].includes(char)) {
+      if (stack.pop() !== pairs[char]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+console.log(isPaired("{[][]}"));
---
+++ transpose/transpose.js
@@ -0,0 +1,59 @@
+﻿/**
+ * Given an input text output it transposed.
+
+Roughly explained, the transpose of a matrix:
+
+ABC
+DEF
+is given by:
+
+AD
+BE
+CF
+Rows become columns and columns become rows. See https://en.wikipedia.org/wiki/Transpose.
+
+If the input has rows of different lengths, this is to be solved as follows:
+
+Pad to the left with spaces.
+Don't pad to the right.
+Therefore, transposing this matrix:
+
+ABC
+DE
+results in:
+
+AD
+BE
+C
+And transposing:
+
+AB
+DEF
+results in:
+
+AD
+BE
+ F
+In general, all characters from the input should also be present in the transposed output. That means that if a column in the input text contains only spaces on its bottom-most row(s), the corresponding output row should contain the spaces in its right-most column(s).
+ */
+
+export const transpose = (input) => {
+  let matrix = [];
+
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      let temp = input[i][j];
+      let rowLength = 0;
+      if (matrix[j]) {
+        rowLength = matrix[j].length;
+      }
+      if (i > rowLength) {
+        temp = temp.padStart(i - rowLength + 1, " ");
+      }
+      matrix[j] ? (matrix[j] += temp) : (matrix[j] = temp);
+    }
+  }
+  return matrix;
+};
+
+console.log(transpose(["abc", "def"]));
